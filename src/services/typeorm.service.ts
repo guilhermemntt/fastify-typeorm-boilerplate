153c2bd45770711dc16bce050fdfbad069678a72
guilhermemntt <guilherme.mnetto@gmail.com>
2020-01-18 23:17:15 -0300

    Services refactoring

--- src/services/typeorm.service.ts
+++ src/services/typeorm.service.ts
@@ -1,11 +1,12 @@
 import { createConnection, DatabaseType, ConnectionOptions } from "typeorm";
 import path from "path";
 import { Service } from "./index.service";
+import fs from "fs";
 
 const typeormService: Service = {
   init: async () => {
     try {
-      await createConnection({
+      let connectionOptions: ConnectionOptions = {
         type: process.env.DB_TYPE as DatabaseType,
         host: process.env.DB_HOST,
         port: Number(process.env.DB_PORT),
@@ -17,11 +18,21 @@ const typeormService: Service = {
           path.resolve(__dirname, "../entities/*.js.map"),
           path.resolve(__dirname, "../entities/*.ts")
         ],
-        synchronize: true
-      } as ConnectionOptions);
+        synchronize: false
+      } as ConnectionOptions;
 
-      console.log("Database service initialized.");
+      if (process.env.DB_SSL == "true")
+        connectionOptions["ssl"] = {
+          key: fs.readFileSync(process.env.DB_SSL_CLIENT_KEY_PATH),
+          cert: fs.readFileSync(process.env.DB_SSL_CLIENT_CERT_PATH),
+          ca: fs.readFileSync(process.env.DB_SSL_CLIENT_CA_PATH)
+        };
+
+      await createConnection(connectionOptions);
+
+      console.log("[TYPEORM] Database service initialized.");
     } catch (error) {
+      console.log("[TYPEORM] Error during database service initialization");
       throw error;
     }
   }
